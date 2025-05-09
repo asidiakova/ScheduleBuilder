083432febc8a499fab7d7fd47ed50a309f1a914a
asidiakova <aleksandra.sidia@gmail.com>
2025-05-09 23:07:41 +0200

    chore: remove unused imports

--- app/src/main/java/com/example/schedulebuilder/ui/theme/Type.kt
+++ app/src/main/java/com/example/schedulebuilder/ui/theme/Type.kt
@@ -1,11 +1,9 @@
 package com.example.schedulebuilder.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
